464ba718fddd7bf9d6ce2c87653890c2ce0e2b32
Harmoen <harmon.the.emu@gmail.com>
2025-03-06 21:24:45 -0600

    Added Raycast to asteroids

--- source/objects/obstacle.gd
+++ source/objects/obstacle.gd
@@ -1,14 +1,19 @@
 class_name Obstacle
 extends Area2D
 
+@onready var ray_cast_2d: RayCast2D = $RayCast2D
+
 # 5000 speed seems to be the max before it's not visible anymore
 @export var speed = 100
 var direction : Vector2 = Vector2(-1,0)
 var damage : float = 10
+var raycast : RayCast2D
 
 
 func _process(delta: float) -> void:
-	self.position += ((minf(Game.speed,5000) + speed) * direction * delta)
+	var last_position = self.global_position
+	self.global_position += ((minf(Game.speed,5000) + speed) * direction * delta)
+	ray_cast_2d.target_position = last_position - self.global_position
 
 
 func on_collision() -> void:
