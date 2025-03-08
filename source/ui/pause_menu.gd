0a2e1a0e038d07be48729d18974131fbf4ea56a5
Harmoen <harmon.the.emu@gmail.com>
2025-03-07 20:50:47 -0600

    Added Volume Sliders

--- source/ui/pause_menu.gd
+++ source/ui/pause_menu.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
 @onready var unpause_bttn: SoundButton = %UnpauseBttn
+@onready var master_volume_slider: HSlider = %MasterVolumeSlider
+@onready var music_volume_slider: HSlider = %MusicVolumeSlider
+@onready var sfx_volume_slider: HSlider = %SfxVolumeSlider
 
 
 func _ready() -> void:
@@ -8,12 +11,19 @@ func _ready() -> void:
 	Game.game_paused.connect(_on_game_paused)
 	Game.game_unpaused.connect(_on_game_unpaused)
 	unpause_bttn.pressed.connect(_on_unpause_button_pressed)
+	master_volume_slider.value_changed.connect(update_audio_bus.bind("Master"))
+	music_volume_slider.value_changed.connect(update_audio_bus.bind("Music"))
+	sfx_volume_slider.value_changed.connect(update_audio_bus.bind("Sound Effects"))
+	master_volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master")))
+	music_volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Music")))
+	sfx_volume_slider.value = db_to_linear(AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Sound Effects")))
 
 
 func _on_game_paused() -> void:
 	AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Music"),0,true)
 	AudioServer.set_bus_effect_enabled(AudioServer.get_bus_index("Music"),1,true)
 	show()
+	unpause_bttn.grab_focus()
 
 
 func _on_game_unpaused() -> void:
@@ -24,3 +34,8 @@ func _on_game_unpaused() -> void:
 
 func _on_unpause_button_pressed() -> void:
 	Game.game_unpaused.emit()
+
+
+func update_audio_bus(new_volume : float = 0.0, bus: StringName = "Master") -> void:
+	new_volume = linear_to_db(new_volume)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), new_volume)
